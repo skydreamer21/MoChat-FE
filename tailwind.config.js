bd40c4ae840dd14441f652314ae67a982b99323b
yeonuy <jyeonuy@gmail.com>
2024-07-30 02:30:37 +0900

    🔧 Chore: TailWindCSS Configuration 초기 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#FFB646",
+      sub: "#FFDF6C",
+      "black-12": "#121212",
+      "black-20": "#202020",
+      "black-60": "#606060",
+      "black-84": "#848484",
+      "gray-dark": "#BDBDBD",
+      "gray-light": "#ECECEC",
+      background: "#F6F6F6",
+      white: "#FDFDFD",
+      red: "#FC6363",
+      blue: "#4F8EEC",
+      green: "#30DA87",
+    },
+    fontFamily: {
+      pre: ["Pretendard"],
+    },
+    extend: {
+      borderRadius: {
+        sm: "4px",
+        md: "8px",
+      },
+    },
   },
   plugins: [],
 };
