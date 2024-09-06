4688908417e082d94e645924044b89e972fd0837
yeonuy <jyeonuy@gmail.com>
2024-09-07 00:05:42 +0900

    ✨ Feat: 그룹 커버 사진 선택 UI

--- src/components/Item/GroupItem.tsx
+++ src/components/Item/GroupItem.tsx
@@ -10,14 +10,12 @@ const GroupItem = ({ groupName, memberCnt, coverImg }: GroupItemProps) => {
   return (
     <div className="flex flex-col gap-1">
       <div className="relative w-full aspect-square rounded-md border border-gray-light">
-        {coverImg ? (
+        {coverImg && (
           <img
             src={coverImg}
             alt={`${groupName} cover img`}
             className="absolute top-0 left-0 w-full h-full object-cover rounded-md"
           />
-        ) : (
-          <div></div>
         )}
       </div>
       <div className="flex flex-col pt-1">
