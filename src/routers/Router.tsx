c266af5e1ef64154b004693cca7d8d957384cbe0
skydreamer <skydreamer210@gmail.com>
2024-09-07 12:52:10 +0900

    chore: workflow check 용 임시 sample 페이지 추가

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -4,6 +4,7 @@ import MainPage from "../pages/MainPage/MainPage";
 import MorePage from "../pages/MorePage/MorePage";
 import FallbackPage from "../pages/FallbackPage/FallbackPage";
 import GroupPage from "../pages/GroupPage/GroupPage";
+import SamplePage from "../pages/SamplePage/SamplePage";
 
 export default function Router() {
   return (
@@ -12,6 +13,7 @@ export default function Router() {
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/more" element={<MorePage />}></Route>
         <Route path="/group" element={<GroupPage />}></Route>
+        <Route path="/sample" element={<SamplePage />}></Route>
         <Route path="/*" element={<FallbackPage />}></Route>
       </Routes>
     </BrowserRouter>
