eae80fa48e9d884481db2c5ea55c201a2d33c854
skydreamer <skydreamer210@gmail.com>
2024-09-07 12:58:20 +0900

    chore: env 임시 삭제

---
+++ src/pages/SamplePage/SamplePage.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../../components/Bar/NavBar";
+
+export default function SamplePage() {
+  return (
+    <>
+      <div>Here is Sample Page</div>
+      <NavBar />
+    </>
+  );
+}
