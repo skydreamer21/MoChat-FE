8cf52b54d3e54ecadb66e74cfd128a135711cffe
yeonuy <jyeonuy@gmail.com>
2024-09-22 18:16:30 +0900

    ✨ Feat: 그룹 생성 완료(성공) UI

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -5,6 +5,7 @@ import MorePage from "../pages/MorePage/MorePage";
 import FallbackPage from "../pages/FallbackPage/FallbackPage";
 import GroupPage from "../pages/GroupPage/GroupPage";
 import NewGroupPage from "../pages/GroupPage/NewGroupPage";
+import GroupSuccessPage from "../pages/GroupPage/GroupSuccessPage";
 
 export default function Router() {
   return (
@@ -14,6 +15,7 @@ export default function Router() {
         <Route path="/more" element={<MorePage />}></Route>
         <Route path="/group" element={<GroupPage />}></Route>
         <Route path="/group/post" element={<NewGroupPage />}></Route>
+        <Route path="/group/success" element={<GroupSuccessPage />}></Route>
         <Route path="/*" element={<FallbackPage />}></Route>
       </Routes>
     </BrowserRouter>
