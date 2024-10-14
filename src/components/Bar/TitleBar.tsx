4194c1e93f0c6306ff01c6f1f53de7a12464ea6f
yeonuy <jyeonuy@gmail.com>
2024-10-14 22:25:44 +0900

    ✨ Feat: TitleBar에 클릭 가능 영역 추가

--- src/components/Bar/TitleBar.tsx
+++ src/components/Bar/TitleBar.tsx
@@ -1,16 +1,33 @@
+import RightArrow from "../../assets/icons/right-arrow.svg?react";
+
 interface TitleBarProps {
   pagename: string;
   isWhite?: boolean;
+  isLink?: boolean;
+  onClick?: () => void;
 }
 
-const TitleBar = ({ pagename, isWhite = true }: TitleBarProps) => {
+const TitleBar = ({
+  pagename,
+  isWhite = true,
+  isLink = false,
+  onClick,
+}: TitleBarProps) => {
   return (
     <div>
       <div
         className={`${isWhite && "bg-white"} relative flex items-center px-5 py-3`}
       >
-        <div className="flex-1 text-left">
-          <span className="font-bold">{pagename}</span>
+        <div className="flex-1 text-left flex items-center cursor-pointer">
+          <span className="font-bold" onClick={isLink ? onClick : undefined}>
+            {pagename}
+          </span>
+          {isLink && (
+            <RightArrow
+              className="ml-2 w-3.5 h-3.5 text-black-60"
+              onClick={onClick}
+            />
+          )}
         </div>
       </div>
     </div>
